1f6c09ec0f5dff9fe8b4dc207fd42caf90a2adb9
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2021-01-11 00:54:17 -0300

    Search order location

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,8 +1,14 @@
 export type Product = {
 
-    id: number,
-    name: string,
-    price: number,
-    description: string,
-    imageUri:string
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri:string;
+}
+
+export type OrderLocationData = {
+    latitude: number;
+    longitude: number;
+    address: string;
 }
